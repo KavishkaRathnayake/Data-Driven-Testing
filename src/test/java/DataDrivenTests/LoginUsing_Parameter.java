c6ae79b245e6264b67a14be06e5fd6ad8ef147de
Kavishka Rathnayake <medhangakavishka06@gmail.com>
2025-02-03 06:30:36 +0530

    Verify Login Using @parameters annotation and xml file

---
+++ src/test/java/DataDrivenTests/LoginUsing_Parameter.java
@@ -0,0 +1,55 @@
+package DataDrivenTests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LoginUsing_Parameter {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void OpenPage(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBrowserVersion("121");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    @Test
+    @Parameters({"Username","Password","Validation"})
+    public void LoginTestScenario(String Uname, String Pswd, String expValidation){
+
+        WebElement Username = driver.findElement(By.xpath("//input[@placeholder='Username']"));
+        Username.sendKeys(Uname);
+
+        WebElement Password = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+        Password.sendKeys(Pswd);
+
+        WebElement loginbutton = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginbutton.click();
+
+        boolean urlVerification =  driver.getCurrentUrl().contains("Dashboard");
+
+        if(expValidation.equals("Valid")) {
+            Assert.assertTrue(urlVerification, "Expected login success, But navigated to the dashboard");
+        }
+        else {
+            Assert.assertFalse(urlVerification, "Expected login fail, But not navigated to the dashboard");
+        }
+    }
+    @AfterMethod
+    public void closeBrowser(){
+        driver.quit();
+    }
+}
